e2d43f231ea8fbbd57cd458a9f74edf9cf413522
Precious Amarachi Onyeukwu <onyeukwuamara@gmail.com>
2023-01-27 15:05:48 +0100

    Update test_Credit_Card_Validator.py

--- test_Credit_Card_Validator.py
+++ test_Credit_Card_Validator.py
@@ -1,47 +1,53 @@
 import unittest
 
+# from import Credit_card_validator
+
 
 class TestCredit_card_validator(unittest.TestCase):
-    Credit_card_validator = ("card_sum", "card_validation", "card_type")
+    credit_card = Credit_card_validator("card_sum", "card_validation", "card_type")
 
 
 
 # add_odd_number_step_3, sum_from_step2_and_3
 
-class CreditCardTest(card):
+class CreditCardTest(unittest.TestCase):
+    
+    def set_up(self):
+        card = ""
+    
     def test_check_card_length(self):
         sup = "438857601252626"
         for i in sup:
-            result = check_card_length(card)
+            result = credit_card.check_card_length(card)
         self.assertTrue(16)
 
     # self.fail()
 
     def test_check_card_type(self):
         print()
-        result2 = check_card_type("4")
+        result2 = credit_card.check_card_type("4")
         self.assertTrue("4")
 
     # self.fail()
 
     def test_add_even_numbers(self):
-        result3 = add_even_numbers([2, 3, 5])
+        result3 = credit_card.add_even_numbers([2, 3, 5])
         inputs = [1, 2, 3, 5]
-        result4 = add_even_numbers(inputs)
+        result4 = credit_card.add_even_numbers(inputs)
         self.assertEqual(result4, result3)
 
     # self.fail()
 
     def test_add_odd_number_step_3(self):
-        result5 = add_odd_number_step_3([3, 7, 9])
+        result5 = credit_card.add_odd_number_step_3([3, 7, 9])
         input2 = [1, 3, 6, 7]
-        result6 = add_odd_number_step_3(input2)
+        result6 = credit_card.add_odd_number_step_3(input2)
         self.assertEqual(result6, result5)
 
     # self.fail()
 
     def test_sum_from_step2_and_3(self):
-        result7 = sum_from_step2_and_3([2, 3, 5])
+        result7 = credit_card.sum_from_step2_and_3([2, 3, 5])
         step4 = 0
         if step4 % 10 == 0:
             print("valid")
